d1e8c4ca494bc87025207694bbbda7fa91109f87
Riku Fudo <riku.fudo@rakus-partners.co.jp>
2022-11-16 17:33:17 +0900

    バリデーションの追加対応

--- src/pages/items/create.tsx
+++ src/pages/items/create.tsx
@@ -14,6 +14,7 @@ type Option = {
   price: number;
 };
 
+// formの型指定
 type FormValues = {
   name: string;
   description: string;
@@ -24,6 +25,7 @@ type FormValues = {
 };
 
 export default function Regist() {
+  // オプションの表示判定state
   const [option1, setOption1] = useState(false);
   const [option2, setOption2] = useState(false);
   const [option3, setOption3] = useState(false);
@@ -32,20 +34,23 @@ export default function Regist() {
     handleSubmit,
     formState: { errors },
     reset,
-  } = useForm<FormValues>({ defaultValues: { deleted: false } });
+  } = useForm<FormValues>({
+    defaultValues: { deleted: false },
+    criteriaMode: 'all',
+  });
 
   // fetch実行関数
   const doFetch: SubmitHandler<FormValues> = (data) => {
     // Number型にキャスト
-    if(!(data.options.length === 0)){
-      data.options.map((option) => {
+    if (!(typeof data.options === 'undefined')) {
+        data.options.map((option) => {
         option.id = Number(option.id);
         option.price = Number(option.price);
-      })
+      });
     }
     data.price = Number(data.price);
     // fetchの実行
-    FetchData(data,'POST').then(() => {
+    FetchData(data, 'POST').then(() => {
       reset();
     });
   };
@@ -59,156 +64,205 @@ export default function Regist() {
             <label id="name">商品名 : </label>
             <input
               id="name"
-              {...register('name', { required: true })}
+              {...register('name', {
+                required: '『商品名』は入力必須の項目です。',
+              })}
             />
-            {errors.name && (
-              <div className="errorMessage">入力が必須の項目です</div>
+            {errors.name?.message && (
+              <div className="errorMessage">
+                {errors.name.message}
+              </div>
             )}
           </li>
           <li>
             <label id="description">説明 : </label>
-            <input id="description" {...register('description')} />
+            <input
+              id="description"
+              {...register('description', {
+                required: '『説明』は入力必須の項目です。',
+              })}
+            />
+            {errors.description?.message && (
+              <div className="errorMessage">
+                {errors.description.message}
+              </div>
+            )}
           </li>
           <li>
             <label id="price">価格：</label>
-            <input id="price" {...register('price')} />
+            <input
+              id="price"
+              {...register('price', {
+                required: {
+                  value: true,
+                  message: '『価格』は入力必須の項目です。',
+                },
+                pattern: {
+                  value: /^[0-9]+$/,
+                  message: '数字のみ入力してください。',
+                },
+              })}
+            />
+            {errors.price?.types?.required && (
+              <div className="errorMessage">
+                {errors.price.types?.required}
+              </div>
+            )}
+            {errors.price?.types?.pattern && (
+              <div className="errorMessage">
+                {errors.price.types.pattern}
+              </div>
+            )}
           </li>
           <li>
             <label id="imageUrl">商品画像URL : </label>
-            <input id="imageUrl" {...register('imageUrl')} />
+            <input
+              id="imageUrl"
+              {...register('imageUrl', {
+                required: '『説明』は入力必須の項目です。',
+              })}
+            />
+            {errors.imageUrl?.message && (
+              <div className="errorMessage">
+                {errors.imageUrl.message}
+              </div>
+            )}
           </li>
         </ul>
+        <section className={'option-wrrapper'}>
+          <div className={'option-item'}>
+            <h2>オプション情報1</h2>
+            <input
+              type="button"
+              value="オプションを設定"
+              onClick={() => setOption1(!option1)}
+            />
+            {option1 && (
+              <ul>
+                <li>
+                  <label id="option-id">ID : </label>
+                  <input
+                    id="option-id"
+                    {...register(`options.${0}.id`)}
+                  />
+                </li>
+                <li>
+                  <label id="option-name">名称 : </label>
+                  <input
+                    id="option-name"
+                    {...register(`options.${0}.name`)}
+                  />
+                </li>
+                <li>
+                  <label id="option-description">説明 : </label>
+                  <input
+                    id="option-description"
+                    {...register(`options.${0}.description`)}
+                  />
+                </li>
+                <li>
+                  <label id="option-price">価格 : </label>
+                  <input
+                    id="option-price"
+                    {...register(`options.${0}.price`)}
+                  />
+                </li>
+              </ul>
+            )}
+          </div>
 
-        <h2>オプション情報1</h2>
-        <button onClick={() => setOption1(true)}>
-          オプションを設定する
-        </button>
-        {option1 ? (
-          <ul>
-            <li>
-              <label id="option-id">ID : </label>
-              <input
-                id="option-id"
-                {...register(`options.${0}.id`)}
-              />
-            </li>
-            <li>
-              <label id="option-name">名称 : </label>
-              <input
-                id="option-name"
-                {...register(`options.${0}.name`)}
-              />
-            </li>
-            <li>
-              <label id="option-description">説明 : </label>
-              <input
-                id="option-description"
-                {...register(`options.${0}.description`)}
-              />
-            </li>
-            <li>
-              <label id="option-price">価格 : </label>
-              <input
-                id="option-price"
-                {...register(`options.${0}.price`)}
-              />
-            </li>
-          </ul>
-        ) : (
-          ''
-        )}
+          <div className={'option-item'}>
+            {option1 && (
+              <>
+                <h2>オプション情報2</h2>
+                <input
+                  type="button"
+                  value="オプションを設定"
+                  onClick={() => setOption2(!option2)}
+                />
+              </>
+            )}
 
-        {option1 ? (
-          <>
-            <h2>オプション情報2</h2>
-            <button onClick={() => setOption2(true)}>
-              オプションを設定する
-            </button>
-          </>
-        ) : (
-          ''
-        )}
+            {option2 && (
+              <ul>
+                <li>
+                  <label id="option-id">ID : </label>
+                  <input
+                    id="option-id"
+                    {...register(`options.${1}.id`)}
+                  />
+                  
+                </li>
+                <li>
+                  <label id="option-name">名称 : </label>
+                  <input
+                    id="option-name"
+                    {...register(`options.${1}.name`)}
+                  />
+                </li>
+                <li>
+                  <label id="option-description">説明 : </label>
+                  <input
+                    id="option-description"
+                    {...register(`options.${1}.description`)}
+                  />
+                </li>
+                <li>
+                  <label id="option-price">価格 : </label>
+                  <input
+                    id="option-price"
+                    {...register(`options.${1}.price`)}
+                  />
+                </li>
+              </ul>
+            )}
+          </div>
+          <div className={'option-item'}>
+            {option2 && (
+              <>
+                <h2>オプション情報3</h2>
+                <input
+                  type="button"
+                  value="オプションを設定"
+                  onClick={() => setOption3(!option3)}
+                />
+              </>
+            )}
 
-        {option2 ? (
-          <ul>
-            <li>
-              <label id="option-id">ID : </label>
-              <input
-                id="option-id"
-                {...register(`options.${1}.id`)}
-              />
-            </li>
-            <li>
-              <label id="option-name">名称 : </label>
-              <input
-                id="option-name"
-                {...register(`options.${1}.name`)}
-              />
-            </li>
-            <li>
-              <label id="option-description">説明 : </label>
-              <input
-                id="option-description"
-                {...register(`options.${1}.description`)}
-              />
-            </li>
-            <li>
-              <label id="option-price">価格 : </label>
-              <input
-                id="option-price"
-                {...register(`options.${1}.price`)}
-              />
-            </li>
-          </ul>
-        ) : (
-          ''
-        )}
-
-        {option2 ? (
-          <>
-            <h2>オプション情報3</h2>
-            <button onClick={() => setOption3(true)}>
-              オプションを設定する
-            </button>
-          </>
-        ) : (
-          ''
-        )}
-
-        {option3 ? (
-          <ul>
-            <li>
-              <label id="option-id">ID : </label>
-              <input
-                id="option-id"
-                {...register(`options.${2}.id`)}
-              />
-            </li>
-            <li>
-              <label id="option-name">名称 : </label>
-              <input
-                id="option-name"
-                {...register(`options.${2}.name`)}
-              />
-            </li>
-            <li>
-              <label id="option-description">説明 : </label>
-              <input
-                id="option-description"
-                {...register(`options.${2}.description`)}
-              />
-            </li>
-            <li>
-              <label id="option-price">価格 : </label>
-              <input
-                id="option-price"
-                {...register(`options.${2}.price`)}
-              />
-            </li>
-          </ul>
-        ) : (
-          ''
-        )}
+            {option3 && (
+              <ul>
+                <li>
+                  <label id="option-id">ID : </label>
+                  <input
+                    id="option-id"
+                    {...register(`options.${2}.id`)}
+                  />
+                </li>
+                <li>
+                  <label id="option-name">名称 : </label>
+                  <input
+                    id="option-name"
+                    {...register(`options.${2}.name`)}
+                  />
+                </li>
+                <li>
+                  <label id="option-description">説明 : </label>
+                  <input
+                    id="option-description"
+                    {...register(`options.${2}.description`)}
+                  />
+                </li>
+                <li>
+                  <label id="option-price">価格 : </label>
+                  <input
+                    id="option-price"
+                    {...register(`options.${2}.price`)}
+                  />
+                </li>
+              </ul>
+            )}
+          </div>
+        </section>
         <button type="submit">登録</button>
       </form>
       <Link href="/">一覧画面へ</Link>
